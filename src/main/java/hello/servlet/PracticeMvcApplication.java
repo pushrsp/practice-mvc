2262ffddc12f43a972bd31cc217df85f13a1aa0f
PUSHRSP <pushrsp@gmail.com>
2023-01-28 00:37:15 +0900

    기본 프로젝트 생성

---
+++ src/main/java/hello/servlet/PracticeMvcApplication.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMvcApplication.class, args);
+    }
+
+}
